e72f84433e4bb596c0f9c47a4378ee2184a3f46c
William Ellis <william.ellis@unit4.com>
2022-12-17 22:01:26 +0100

    Add doc comment

--- AzPipelineVariable.psm1
+++ AzPipelineVariable.psm1
@@ -1,3 +1,12 @@
+<#
+.SYNOPSIS
+    Sets a variable in an Azure Pipelines pipeline.
+.DESCRIPTION
+    The variable is set using the task.setvariable command. It will be read-only
+    by default.
+.LINK
+    https://learn.microsoft.com/en-us/azure/devops/pipelines/process/set-variables-scripts
+#>
 function Set-AzPipelineVariable {
     Write-Host '##vso[task.setvariable variable=foo]xyz'
 }
