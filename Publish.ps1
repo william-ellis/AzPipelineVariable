9097311c4243487b47f245bf80c14ebea1a029bd
William Ellis <william.ellis@unit4.com>
2022-12-18 12:11:55 +0100

    Add publish script

---
+++ Publish.ps1
@@ -0,0 +1,19 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)]
+    [string]$Repository
+)
+
+Remove-Item .\output\* -Force -Recurse -ErrorAction Ignore
+New-Item .\output\AzPipelineVariable -ItemType Directory | Out-Null
+
+Copy-Item AzPipelineVariable.psd1 output\AzPipelineVariable
+Copy-Item AzPipelineVariable.psm1 output\AzPipelineVariable
+Copy-Item .\LICENSE output\AzPipelineVariable
+Copy-Item .\README.md output\AzPipelineVariable
+Copy-Item .\CHANGELOG.md output\AzPipelineVariable
+
+Publish-Module `
+    -Name .\output\AzPipelineVariable `
+    -Repository $Repository `
+    -AllowPrerelease -SkipAutomaticTags
