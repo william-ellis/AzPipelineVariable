fdab89f4707dd04f56ece6f46cdbed3bee49165b
William Ellis <william.ellis@unit4.com>
2022-12-19 13:39:36 +0100

    Refactor

--- Publish.ps1
+++ Publish.ps1
@@ -1,8 +1,8 @@
 Remove-Item .\output\* -Force -Recurse -ErrorAction Ignore
-New-Item .\output\AzPipelineVariable -ItemType Directory | Out-Null
+New-Item .\output -ItemType Directory | Out-Null
 
-Copy-Item AzPipelineVariable.psd1 output\AzPipelineVariable
-Copy-Item AzPipelineVariable.psm1 output\AzPipelineVariable
-Copy-Item .\LICENSE output\AzPipelineVariable
-Copy-Item .\README.md output\AzPipelineVariable
-Copy-Item .\CHANGELOG.md output\AzPipelineVariable
+Copy-Item AzPipelineVariable.psd1 output
+Copy-Item AzPipelineVariable.psm1 output
+Copy-Item .\LICENSE output
+Copy-Item .\README.md output
+Copy-Item .\CHANGELOG.md output
