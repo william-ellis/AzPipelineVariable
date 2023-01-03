c45eff968ed4bb52abe56bb2500a96c74d47aa4d
william-ellis <w.ellis1483@gmail.com>
2023-01-03 12:45:16 +0100

    Clean up build script

--- Build.ps1
+++ Build.ps1
@@ -3,6 +3,6 @@ New-Item .\output -ItemType Directory | Out-Null
 
 Copy-Item AzPipelineVariable.psd1 output
 Copy-Item AzPipelineVariable.psm1 output
-Copy-Item .\LICENSE output
-Copy-Item .\README.md output
-Copy-Item .\CHANGELOG.md output
+Copy-Item LICENSE output
+Copy-Item README.md output
+Copy-Item CHANGELOG.md output
